2f2886969f0eba0879e6dd885d941ce754d4948b
DWC97 <dwcarton1@gmail.com>
2023-11-14 16:57:22 +0000

    filtered search through player data

---
+++ src/SuggestedSearch.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import data from "./final.json"
+
+export function SuggestedSearch(){
+
+    const [value, setValue] = useState("")
+    const list = data.results
+
+    function onSearch(player){
+        setValue(player)
+    }
+
+    return (
+        <div>
+            <h1>Search</h1>
+            <br/>
+            <br/>
+            <input type="text" value={value} onChange={(e) => {
+                setValue(e.target.value)
+            }}/>
+            {list.filter(item => {
+                return value && item.long_name.toLowerCase().includes(value.toLowerCase())
+            })
+            .map(item => {
+                return <div key={item.player_id} onClick={() => {
+                    onSearch(item.short_name)
+                }}>{`${item.short_name} - ${item.overall}`}</div>
+            })}
+        </div>
+    )
+}
