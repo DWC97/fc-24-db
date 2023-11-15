8558fbbbeb818513cf5d59ef5f2df370449396f8
DWC97 <dwcarton1@gmail.com>
2023-11-15 16:43:39 +0000

    404 error if player doesn't exist

--- src/PlayerPage.jsx
+++ src/PlayerPage.jsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom"
+import data from "./final.json"
 
 export function PlayerPage(){
 
     const { id } = useParams()
+    const list = data.results
+    const isPlayer = list.find(item => {
+        return item.short_name === id
+    })
 
     return (
         <div>
-            {id}
-        </div>
+            {isPlayer ? <div>{id}</div> : <div>404</div>}
+        </div> 
     )
 }
