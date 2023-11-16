e999465f1e22a146d5e347c81f4c07a6cdb2746c
DWC97 <dwcarton1@gmail.com>
2023-11-16 12:39:19 +0000

    useCallback to find last player in list

--- src/PlayerList.jsx
+++ src/PlayerList.jsx
@@ -1,30 +1,45 @@
 import { Link } from "react-router-dom"
 import data from "./final.json"
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
 export function PlayerList(){
 
     const list = data.results
+
     const [currentPage, setCurrentPage] = useState(1);
     const [recordsPerPage] = useState(40);
 
     const indexOfLastRecord = currentPage * recordsPerPage;
     const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
 
-    const currentRecords = list.slice(indexOfFirstRecord, 
-        indexOfLastRecord);
+    const currentRecords = list.slice(indexOfFirstRecord,indexOfLastRecord);
     
-    console.log(currentRecords)
+    const observer = useRef()
+    const lastPlayerElementRef = useCallback(node => {
+        console.log(node)
+    })
 
     return (
         <div className="player-list">
-            {currentRecords.map(item => {
-                return (
-                <div key={item.player_id}>
-                    <Link to={`/player/${item.short_name}`}>
-                        {`${item.short_name} - ${item.overall}`}
-                    </Link>   
-                </div>)
+            {currentRecords.map((item, index) => {
+                if (currentRecords.length === index + 1){
+                    return (
+                        <div key={item.player_id} ref={lastPlayerElementRef}>
+                            <Link to={`/player/${item.short_name}`}>
+                            {`${item.short_name} - ${item.overall}`}
+                            </Link>   
+                        </div>)
+                    
+                }
+                else {
+                    return (
+                        <div key={item.player_id}>
+                            <Link to={`/player/${item.short_name}`}>
+                                {`${item.short_name} - ${item.overall}`}
+                            </Link>   
+                        </div>)
+                }
+                
             })}
         </div>
     )
