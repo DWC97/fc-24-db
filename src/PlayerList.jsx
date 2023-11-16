8c63bff18de9bc7df77f4e54b7ba3bbb9c4e3309
DWC97 <dwcarton1@gmail.com>
2023-11-16 12:04:01 +0000

    sliced dataest

--- src/PlayerList.jsx
+++ src/PlayerList.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom"
 import data from "./final.json"
+import { useState } from "react";
 
 export function PlayerList(){
 
     const list = data.results
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recordsPerPage] = useState(40);
+
+    const indexOfLastRecord = currentPage * recordsPerPage;
+    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+
+    const currentRecords = list.slice(indexOfFirstRecord, 
+        indexOfLastRecord);
+    
+    console.log(currentRecords)
 
     return (
         <div className="player-list">
-            {list.map(item => {
+            {currentRecords.map(item => {
                 return (
                 <div key={item.player_id}>
                     <Link to={`/player/${item.short_name}`}>
