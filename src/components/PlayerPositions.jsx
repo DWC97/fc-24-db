cfa494b85539415262b41bd5740b658d3367d502
DWC97 <dwcarton1@gmail.com>
2023-11-22 16:23:00 +0000

    logic for checked input box

--- src/components/PlayerPositions.jsx
+++ src/components/PlayerPositions.jsx
@@ -1,15 +1,91 @@
+import { useState } from "react"
+
 export function PlayerPositions(){
 
-    const playerPositions = ["ST", "LW", "CM", "CAM", "CF", "CDM", "CB", "RW", "LM", "LWB", "LB", "RM", "RWB", "RB"]
+    const playerPositions = [
+        {
+            "position": "ST",
+            "checked": false
+        },
+        {
+            "position": "CF",
+            "checked": false
+        },
+        {
+            "position": "LW",
+            "checked": false
+        },
+        {
+            "position": "LM",
+            "checked": false
+        },
+        {
+            "position": "RW",
+            "checked": false
+        },
+        {
+            "position": "RM",
+            "checked": false
+        },
+        {
+            "position": "CAM",
+            "checked": false
+        },
+        {
+            "position": "CM",
+            "checked": false
+        },
+        {
+            "position": "CDM",
+            "checked": false
+        },
+        {
+            "position": "LWB",
+            "checked": false
+        },
+        {
+            "position": "LB",
+            "checked": false
+        },
+        {
+            "position": "RWB",
+            "checked": false
+        },
+        {
+            "position": "RB",
+            "checked": false
+        },
+        {
+            "position": "CB",
+            "checked": false
+        }
+    ]
+    const [positions, setPositions] = useState(playerPositions)
+
+    function filterByPosition(position){
+        setPositions(prev => {
+            return prev.map(p => {
+                if (p === position){
+                    return {...p, checked: !p.checked}
+                }
+                else {
+                    return {...p, checked: false}
+                }
+            })
+        })
+    }
 
     return (
         <div>
-            {playerPositions.map(position => {
+            {positions.map(p => {
                 return (
                         <label 
-                        key={position}>
-                            <input type="checkbox" />
-                            {position}
+                        key={p.position}>
+                            <input 
+                            type="checkbox" 
+                            checked={p.checked}
+                            onChange={() => filterByPosition(p)}/>
+                            {p.position}
                         </label>
                         )
             })}
