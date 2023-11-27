ae6fa50c15f29f9528b3cd7cb7436b8c6e44727d
DWC97 <dwcarton1@gmail.com>
2023-11-27 17:22:15 +0000

    infinite scroll

--- src/NewPlayerList.jsx
+++ src/NewPlayerList.jsx
@@ -1,93 +1,126 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { Player } from "./Player";
 
 export function NewPlayerList({ players }){
     const [positionFilter, setPositionFilter] = useState('');
-  const [sortedBy, setSortedBy] = useState('');
-  const [sortOrder, setSortOrder] = useState('desc');
+    const [sortedBy, setSortedBy] = useState('');
+    const [sortOrder, setSortOrder] = useState('desc');
+    const [currentPage, setCurrentPage] = useState(1);
 
-  const toggleSortOrder = (stat) => {
-    if (sortedBy === stat) {
-      setSortOrder(prevOrder => (prevOrder === 'asc' ? 'desc' : 'asc'));
-    } else {
-      setSortOrder('desc');
-    }
-    setSortedBy(stat);
-  };
+    const toggleSortOrder = (stat) => {
+        if (sortedBy === stat) {
+        setSortOrder(prevOrder => (prevOrder === 'asc' ? 'desc' : 'asc'));
+        } else {
+        setSortOrder('desc');
+        }
+        setSortedBy(stat);
+    };
 
-  const sortedPlayers = players
-    .filter(player => !positionFilter || positionFilter === 'ALL' || player.player_positions.includes(positionFilter))
-    .sort((a, b) => {
-      const orderFactor = sortOrder === 'asc' ? 1 : -1;
-      return (a[sortedBy] - b[sortedBy]) * orderFactor;
-    });
+    const sortedPlayers = players
+        .filter(player => !positionFilter || positionFilter === 'ALL' || player.player_positions.includes(positionFilter))
+        .sort((a, b) => {
+        const orderFactor = sortOrder === 'asc' ? 1 : -1;
+        return (a[sortedBy] - b[sortedBy]) * orderFactor;
+        })
+        .slice(0, 40 * currentPage);
 
-//   const uniquePositions = [...new Set(players.map(player => player.club_position))];
+    //   const uniquePositions = [...new Set(players.map(player => player.club_position))];
+    
+    //   const positionOptions = ['ALL', ...uniquePositions];
+    const playerPositions = ["ALL", "ST", "CF", "LW", "LM", "RW", "RM", "CAM", "CM", "CDM", "LWB", "LB", "RWB", "RB", "CB"]
+    const playerStats = [
+        {
+            "name": "overall",
+            "abbreviation": "OVE"
+
+        },
+        {
+            "name": "pace",
+            "abbreviation": "PAC"
+        },
+        {
+            "name": "shooting",
+            "abbreviation": "SHO"
+        },
+        {
+            "name": "passing",
+            "abbreviation": "PAS"
+        },
+        {
+            "name": "dribbling",
+            "abbreviation": "DRI"
+        },
+        {
+            "name": "defending",
+            "abbreviation": "DEF"
+        },
+        {
+            "name": "physic",
+            "abbreviation": "PHY"
+        }
+    ]
+
+    const observer = useRef()
+    const lastPlayerElementRef = useCallback(node => {
   
-//   const positionOptions = ['ALL', ...uniquePositions];
-  const playerPositions = ["ALL", "ST", "CF", "LW", "LM", "RW", "RM", "CAM", "CM", "CDM", "LWB", "LB", "RWB", "RB", "CB"]
-  const playerStats = [
-    {
-        "name": "overall",
-        "abbreviation": "OVE"
+        if (observer.current) observer.current.disconnect()
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting){
+                console.log("observed")
+                setCurrentPage(prevNumber => prevNumber + 1)
+                console.log(currentPage)
+            }
+        })
 
-    },
-    {
-        "name": "pace",
-        "abbreviation": "PAC"
-    },
-    {
-        "name": "shooting",
-        "abbreviation": "SHO"
-    },
-    {
-        "name": "passing",
-        "abbreviation": "PAS"
-    },
-    {
-        "name": "dribbling",
-        "abbreviation": "DRI"
-    },
-    {
-        "name": "defending",
-        "abbreviation": "DEF"
-    },
-    {
-        "name": "physic",
-        "abbreviation": "PHY"
-    }
-]
+        if (node) observer.current.observe(node)
+    }, [])
 
-  return (
-    <div>
-      <label>
-        Filter by Position:
-        <select value={positionFilter} onChange={e => setPositionFilter(e.target.value)}>
-          {playerPositions.map(position => (
-            <option key={position} value={position}>
-              {position}
-            </option>
-          ))}
-        </select>
-      </label>
+    return (
+        <div>
+        <label>
+            Filter by Position:
+            <select value={positionFilter} onChange={e => setPositionFilter(e.target.value)}>
+            {playerPositions.map(position => (
+                <option key={position} value={position}>
+                {position}
+                </option>
+            ))}
+            </select>
+        </label>
 
-      <div>
-        {playerStats.map(stat => (
-          <span
-            key={stat.name}
-            style={{ cursor: 'pointer', marginRight: '10px', fontWeight: sortedBy === stat.name ? 'bold' : 'normal' }}
-            onClick={() => toggleSortOrder(stat.name)}
-          >
-            {stat.abbreviation}
-          </span>
-        ))}
-      </div>
+        <div>
+            {playerStats.map(stat => (
+            <span
+                key={stat.name}
+                style={{ cursor: 'pointer', marginRight: '10px', fontWeight: sortedBy === stat.name ? 'bold' : 'normal' }}
+                onClick={() => toggleSortOrder(stat.name)}
+            >
+                {stat.abbreviation}
+            </span>
+            ))}
+        </div>
 
-      <div>
-        {sortedPlayers.map(player => (
-          <Player key={player.player_id} {...player} />
-        ))}
-      </div>
-    </div>
-  );
+        <div>
+            {/* {sortedPlayers.map((player, index) => (
+            <Player key={player.player_id} {...player} />
+            ))} */}
+            {sortedPlayers.map((player, index) => {
+                if (sortedPlayers.length === index + 1){
+                    return (
+                        <div ref={lastPlayerElementRef} key={player.player_id}>
+                            <Player  {...player} />
+                        </div>
+                    )
+                }
+                else {
+                    return (
+                        <div key={player.player_id}>
+                            <Player  {...player} />
+                        </div>
+                    )
+                }
+            })}
+        </div>
+        </div>
+    );
 }
