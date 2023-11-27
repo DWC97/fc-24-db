b667b5a1f9e6523da574e0af7bca11842b73d79d
DWC97 <dwcarton1@gmail.com>
2023-11-27 15:52:39 +0000

    filter by position working

--- src/NewPlayerList.jsx
+++ src/NewPlayerList.jsx
@@ -16,15 +16,16 @@ export function NewPlayerList({ players }){
   };
 
   const sortedPlayers = players
-    .filter(player => !positionFilter || positionFilter === 'ALL' || player.position === positionFilter)
+    .filter(player => !positionFilter || positionFilter === 'ALL' || player.player_positions.includes(positionFilter))
     .sort((a, b) => {
       const orderFactor = sortOrder === 'asc' ? 1 : -1;
       return (a[sortedBy] - b[sortedBy]) * orderFactor;
     });
 
-  const uniquePositions = [...new Set(players.map(player => player.club_position))];
+//   const uniquePositions = [...new Set(players.map(player => player.club_position))];
   
-  const positionOptions = ['ALL', ...uniquePositions];
+//   const positionOptions = ['ALL', ...uniquePositions];
+  const playerPositions = ["ALL", "ST", "CF", "LW", "LM", "RW", "RM", "CAM", "CM", "CDM", "LWB", "LB", "RWB", "RB", "CB"]
   const playerStats = [
     {
         "name": "overall",
@@ -62,7 +63,7 @@ export function NewPlayerList({ players }){
       <label>
         Filter by Position:
         <select value={positionFilter} onChange={e => setPositionFilter(e.target.value)}>
-          {positionOptions.map(position => (
+          {playerPositions.map(position => (
             <option key={position} value={position}>
               {position}
             </option>
