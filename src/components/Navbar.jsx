7b5611fd34881f856118cdaa39adcf806a052413
DWC97 <dwcarton1@gmail.com>
2023-12-05 15:34:35 +0000

    basic navbar with logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export function Navbar(){
     return (
-        <div className="w-full bg-custom-grey h-4 text-blue-500">
-            <div>
-                hi
+        <div className="w-full bg-custom-grey h-20 flex justify-between fixed">
+            <div className="px-8 flex items-center bg-custom-maroon">
+                <a href="/"><img src="./assets/logos/nav.png" className="w-12"/></a>
             </div>
         </div>
     )
