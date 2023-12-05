1a58497a94b9d392102692533993c69a90ac03cf
DWC97 <dwcarton1@gmail.com>
2023-12-05 17:50:53 +0000

    active nav class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Icon } from '@iconify/react';
 
 export function Navbar(){
     return (
         <div className="w-full bg-custom-grey h-20 flex fixed justify-between">
             <div className="flex ">
-                <div className="w-32 flex items-center justify-center ">
-                    <Link to={"/"}>
+                <NavLink to={"/"}>
+                    <div className="w-32 flex items-center justify-center h-full">
                         <img src="assets/logos/nav.png" className="w-12"/>
-                    </Link>
-                </div>
+                    </div>
+                </NavLink>
                 <ul className="flex flex-row">
-                    <Link to={"/players"}>
-                        <li className="text-white w-32 bg-custom-maroon h-full flex items-center justify-center font-medium">
+                    <NavLink to={"/players"}>
+                        <li className="text-white w-32 h-full flex items-center justify-center font-medium">
                             Players
                         </li>
-                    </Link>
+                    </NavLink>
                     <li className="text-white w-32 h-full flex items-center justify-center font-medium">
                         Nations
                     </li>
