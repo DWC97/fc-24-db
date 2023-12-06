c5bc36c4dfb164562762c9d1fb3787ceddabab97
DWC97 <dwcarton1@gmail.com>
2023-12-06 13:50:35 +0000

    Home page markup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
 export function Home(){
     return (
-        <div>
-            
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <div>
+                <img src="assets/logos/main.png" className="w-96"/>
+            </div>
+            <div>
+                THE ULTIMATE PLAYER DATABASE
+            </div>
+            <div>
+                <input type="text" placeholder="Search player name..." />
+            </div>
+            <div className="flex flex-row">
+                <div className="flex flex-col">
+                    <img src="assets/images/champions.jpg" className="w-48 h-24"/>
+                    <span>MATCHDAY 5 RESULTS</span>
+                </div>
+            </div>
+            <div>
+                Please note this is not a commercial product and is only hosted as part of a web development portfolio.
+            </div>
         </div>
     )
 }
