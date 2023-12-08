e4c4cd8fa6c30430160743e82fe4db365f1ae5c8
DWC97 <dwcarton1@gmail.com>
2023-12-08 17:25:42 +0000

    hover effect for dropdown item

--- src/components/PlayerSearch.jsx
+++ src/components/PlayerSearch.jsx
@@ -36,14 +36,14 @@ export function PlayerSearch({ short_name, overall, player_id, club_team_id, nat
         <Link to={`/player/${short_name}`}>
             <div onClick={() => {
             onSearch(short_name)
-            }} className="flex flex-row relative bg-gray-50 items-center w-96 justify-between py-1 border-b border-gray-300">
+            }} className="flex flex-row relative bg-gray-50 items-center w-96 justify-between py-1 border-b border-gray-300 text-custom-maroon hover:bg-custom-maroon hover:text-white">
                 <div className="pl-4 flex flex-row items-center justify-center">
                     <img src={`https://cdn.sofifa.net/players/231/747/24_120.png`} className="w-7"/>
                     <img src={`https://cdn.sofifa.net/meta/team/9/120.png`} className="w-5 h-5 ml-6 mr-8"/>
                     <img src={`https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/800px-Flag_of_France.svg.png`} className="w-6 h-4"/>
                 </div>
-                <span className="text-xs text-custom-maroon font-medium absolute ml-48">{short_name}</span>
-                <span className="pr-4 font-semibold text-custom-maroon">{overall}</span>
+                <span className="text-xs font-medium absolute ml-48">{short_name}</span>
+                <span className="pr-4 font-semibold ">{overall}</span>
             </div>
         </Link>
         
