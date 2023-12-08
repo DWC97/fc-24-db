a83df6ee1d6c2effde004a3a20ae297477eba710
DWC97 <dwcarton1@gmail.com>
2023-12-08 11:47:06 +0000

    basic logic for adding player headshot

--- src/components/PlayerSearch.jsx
+++ src/components/PlayerSearch.jsx
@@ -1,25 +1,32 @@
 import { Link } from "react-router-dom"
-import { data } from "../data/male_teams.json"
+// import { data } from "../data/male_teams.json"
 
-export function PlayerSearch({ short_name, overall, player_url, club_team_id, nation_team_id}){
+export function PlayerSearch({ short_name, overall, player_id, club_team_id, nation_team_id}){
 
-    const badgeUrl = data.find(item => {
-        if (item.team_id === club_team_id){
-            return item.team_url
-        }
-        else {
-            return "/team/1/arsenal/240002"
-        }
-    })
+    // const badgeUrl = data.find(item => {
+    //     if (item.team_id === club_team_id){
+    //         return item.team_url
+    //     }
+    //     else {
+    //         return "/team/1/arsenal/240002"
+    //     }
+    // })
 
-    const flagUrl = data.find(item => {
-        if (item.team_id === nation_team_id){
-            return item.team_url
-        }
-        else {
-            return "/team/1318/england/240002"
-        }
-    })
+    // const flagUrl = data.find(item => {
+    //     if (item.team_id === nation_team_id){
+    //         return item.team_url
+    //     }
+    //     else {
+    //         return "/team/1318/england/240002"
+    //     }
+    // })
+
+    function splitId(id){
+        console.log(id)
+        const firstId = id.slice(0, 3)
+        const secondId = id.slice(3, 6)
+        return `${firstId}/${secondId}`
+    }
 
     function onSearch(player){
         setValue(player)
@@ -30,7 +37,7 @@ export function PlayerSearch({ short_name, overall, player_url, club_team_id, na
             <div onClick={() => {
             onSearch(short_name)
             }}>
-                <img src=""/>
+                <img src={`https://cdn.sofifa.net/players/${splitId("231747")}/24_120.png`}/>
                 {`${short_name} - ${overall}`}
             </div>
         </Link>
