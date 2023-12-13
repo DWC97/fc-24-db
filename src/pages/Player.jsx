69a64110a28ba81c2550d33e0e6dd753b255224b
DWC97 <dwcarton1@gmail.com>
2023-12-13 11:20:25 +0000

    banner responsiveness

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -10,27 +10,28 @@ export function Player({ players }){
     console.log(player)
 
     return (
-        <div className="flex flex-col w-full px-20">
+        <div className="flex flex-col w-full px-5 md:px-10 lg:px-20">
            <div className="flex flex-row mt-36 relative justify-between items-center text-custom-maroon">
                 <div className="bg-gray-200 rounded-full overflow-hidden">
-                    <img src="https://cdn.sofifa.net/players/231/747/24_120.png" className="w-32"/>
+                    <img src="https://cdn.sofifa.net/players/231/747/24_120.png" className="w-24 md:w-32"/>
                 </div>
-                <div className="absolute flex flex-col justify-around h-full ml-40 py-4">
-                    <span className="text-3xl font-medium tracking-widest">{player.short_name}</span>
-                    <div className="flex flex-row justify-between items-center">
+                <div className="absolute flex flex-col justify-around h-full ml-28 md:ml-40 py-4">
+                    <span className="hidden md:flex md:text-3xl font-medium tracking-widest">{player.short_name}</span>
+                    <span className="text-xl flex md:hidden">{player.short_name}  <span className="font-semibold ml-3">{player.overall}</span></span>
+                    <div className="flex flex-row justify-between items-center text-xs md:text-base">
                         <div className="flex flex-row items-center">
                             <span>{player.club_name.toUpperCase()}</span>
-                            <img src="https://cdn.sofifa.net/meta/team/9/120.png" className="w-7 ml-3"/>
+                            <img src="https://cdn.sofifa.net/meta/team/9/120.png" className="w-4 ml-1 md:w-7 md:ml-3"/>
                         </div>
-                        <div className="flex flex-row ml-8 items-center">
+                        <div className="flex flex-row ml-2 md:ml-8 items-center">
                             <span>{player.nationality_name.toUpperCase()}</span>
-                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/800px-Flag_of_France.svg.png" className="w-7 ml-3"/>
+                            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/800px-Flag_of_France.svg.png" className="w-4 ml-1 md:w-7 md:ml-3"/>
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-row justify-center items-center">
-                    <span className="text-2xl mr-6">{player.club_position}</span>
-                    <span className="text-6xl font-semibold">{player.overall}</span>
+                <div className="hidden md:flex flex-row justify-center items-center">
+                    <span className="text-lg mr-2 md:text-2xl md:mr-6">{player.club_position}</span>
+                    <span className="text-4xl md:text-6xl font-semibold">{player.overall}</span>
                 </div>
            </div>
         </div>
