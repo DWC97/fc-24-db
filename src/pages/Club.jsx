4273ab4a950ffc91e1f1491097a64fc39b5afe4f
DWC97 <dwcarton1@gmail.com>
2023-12-31 16:58:23 +0000

    styling for club banner

--- src/pages/Club.jsx
+++ src/pages/Club.jsx
@@ -8,9 +8,9 @@ export function Club(){
 
     return (
         <div className="w-full flex flex-col px-5 md:px-10 lg:px-20">
-            <div className="flex flex-col mt-36 h-96 bg-blue-600">
-                <h1 className="text-custom-maroon text-2xl font-semibold tracking-widest mb-4">{id}</h1>
-
+            <div className="flex flex-row items-center mt-32 bg-custom-grey h-24">
+                <img src="https://cdn.sofifa.net/meta/team/9/120.png" className="w-16 mx-8"/>
+                <h1 className="text-white text-3xl font-medium tracking-widest">CLUB - {id.toUpperCase()}</h1>
             </div>
         </div>
     )
