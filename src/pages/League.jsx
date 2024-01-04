ad4d078487343e41a48b76e00d3247cde009a65a
DWC97 <dwcarton1@gmail.com>
2024-01-04 15:50:48 +0000

    league banner color

--- src/pages/League.jsx
+++ src/pages/League.jsx
@@ -2,29 +2,45 @@ import { useParams } from "react-router-dom"
 import leagueData from "../data/leagues.json"
 import { Link } from "react-router-dom"
 import { Icon } from '@iconify/react';
+import { useState } from "react";
 
 export function League(){
 
     const { id } = useParams()
     const league = leagueData.leagues.find(league => league.name === id)
-    console.log(league)
+    console.log(league.color)
+    const [IsHovering, setIsHovering] = useState(false)
+
+    function setBg(){
+
+    }
+
+    function bgGenerator(color){
+        const randcolor = "hover:bg-[#0057B8]"
+        console.log(color)
+        // return `hover:bg-[${randcolor}]`
+        return color
+    }
 
     return (
         <div className="w-full flex flex-col px-5 md:px-10 lg:px-20">
-            <div className="flex flex-row items-center mt-32 bg-custom-grey h-16 md:h-24">
+            <div className={`flex flex-row items-center mt-32  h-16 md:h-24`}
+            style={{backgroundColor: `${league.color}`}}>
                 <img src={league.url} className="w-10 md:w-12 mx-4 md:mx-8"/>
                 <h1 className="text-white text-sm md:text-3xl font-medium tracking-widest">{league.name.toUpperCase()}</h1>
             </div>
-            <div className="grid grid-cols-4 gap-4 mt-8">
+            <div className="grid grid-cols-4 gap-4 mt-8 mb-4">
                 {league.clubs.map(club => {
                     return (
                     <Link to={`/clubs/${club.name}`}>
-                        <div key={club.name} className="flex flex-col justify-between bg-gray-100 border-b-2 border-custom-grey rounded-3xl text-custom-grey hover:text-white h-44 hover:scale-110 ease-in-out duration-300 hover:bg-custom-maroon">
+                        <div key={club.name} className={`flex flex-col justify-between  border-b-2 border-custom-grey rounded-3xl text-custom-grey hover:text-white h-44 hover:scale-110 ease-in-out duration-300`}     
+                        style={IsHovering ? {backgroundColor: "lightblue"} : {backgroundColor: "#f5f5f5"}}
+                        >
                             <img src={club.url} className="w-28 pt-3 pl-3"/>
                             <div className="flex flex-row pb-2 items-center justify-between">
-                                <span className="pl-6 tracking-widest font-semibold">{club.name.toUpperCase()}</span>
+                                <span className={`pl-6 tracking-widest font-semibold`}>{club.name.toUpperCase()}</span>
                                 <div className="mr-6">
-                                    <Icon icon="cil:arrow-right" color="white" />
+                                    <Icon icon="cil:arrow-right" color="#f5f5f5" />
                                 </div>
                             </div>
                         </div>
