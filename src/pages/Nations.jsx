25464505e7842c5cca95324bc20524d4af58e378
DWC97 <dwcarton1@gmail.com>
2024-01-08 13:57:05 +0000

    nations with flag render

--- src/pages/Nations.jsx
+++ src/pages/Nations.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
-import data from "../data/male_teams.json"
+import nationsData from "../data/male_teams.json"
+import newNationsData from "../data/nations.json"
 
 export function Nations(){
 
     const [value, setValue] = useState("")
-    const nationsData = data.results.filter(item => item.league_name === "Friendly International" 
+    const nationsArray = nationsData.results.filter(item => item.league_name === "Friendly International" 
     ).sort(compareByName)
-    console.log(removeDuplicates(nationsData, it => it.team_name))
+    const filteredNationsData = removeDuplicates(nationsArray, it => it.team_name)
 
     function compareByName(a, b) {
         return a.team_name.localeCompare(b.team_name);
@@ -26,6 +27,16 @@ export function Nations(){
                 <h1 className="text-custom-maroon text-2xl font-semibold tracking-widest mb-4">NATIONS</h1>
                 <input type="text" placeholder="Search nation..." className="border-b-2 border-custom-grey py-2  font-medium text-custom-grey w-72 md:w-96 outline-none" value={value} onChange={(e) => {
                 setValue(e.target.value)}}/>
+            </div>
+            <div>
+                {newNationsData.map((nation, index) => {
+                    return (
+                        <div key={index} className="flex flex-row">
+                            <img src={nation.code.length > 2 ? nation.code : `https://flagsapi.com/${nation.code}/flat/64.png`} className="w-10"/>
+                            <span>{nation.name}</span>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
