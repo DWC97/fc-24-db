dafae4acc1740cc540c92d1f262355e250a13b20
DWC97 <dwcarton1@gmail.com>
2024-01-24 11:27:25 +0000

    404 page responsive

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,10 +6,10 @@ export function NotFound(){
             <span className="mt-56 text-custom-maroon font-semibold tracking-wider text-7xl">
                 404
             </span>
-            <span className="mt-2 mb-8 text-custom-maroon font-medium text-2xl">
+            <span className="mt-2 mb-8 text-custom-maroon font-medium text-lg md:text-2xl px-8 text-center">
                 The page you were looking for doesn't exist...
             </span>
-            <NavLink to={"/"}><span className="hover:bg-custom-maroon text-custom-maroon border-2 border-custom-maroon hover:text-white hidden md:flex p-2 md:px-4 md:py-2 rounded-md cursor-pointer text-xs md:text-base ease-in-out duration-300" >
+            <NavLink to={"/"}><span className="hover:bg-custom-maroon text-custom-maroon border-2 border-custom-maroon hover:text-white flex px-4 py-2 rounded-md cursor-pointer text-base ease-in-out duration-300" >
                 Return to home
             </span></NavLink>
         </div>
